8ff5039a2227c37497ad0abfa9cc19b9a348c7c5
Taras <slipetstp83@gmail.com>
2020-05-22 19:59:32 +0300

    Finish Home work_18_function_practic

--- lessons/Lesson_15_practic_cycle_condition.js
+++ lessons/Lesson_15_practic_cycle_condition.js
@@ -76,7 +76,7 @@ if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
     console.log("Вы классический зритель");
-} else if (personalMovieDB.count <= 30){
+} else if (personalMovieDB.count >= 30){
     console.log("Вы киноман");
 } else {
     console.log("Произошла ошибка");
